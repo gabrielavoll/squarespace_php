a0508c190d5f4348b4afa6b039a1b14a1251acc4
Gabriela Voll <gabriela.voll@youvisit.com>
2019-12-28 16:14:30 -0500

    successfully utilzing componser

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+	require_once dirname(__DIR__) . '/vendor/autoload.php';
+	$request = $_SERVER['REQUEST_URI'];
+
+	if($request == '/' || $request == ''){
+		require dirname(__DIR__)  . '/html/index.html';
+	} else if ( preg_match('%^\/jokes(\?.*)?$%', $request) ){
+		require dirname(__DIR__) . '/src/jokes.php';
+	} else {
+		http_response_code(404);
+	  require dirname(__DIR__)  . '/html/404.html';
+	}
+?>
