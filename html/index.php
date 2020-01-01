7a20f4813b2f618fcdbad83bfbefa4692a5b482c
Gabriela Voll <gabriela.voll@youvisit.com>
2019-12-31 19:27:48 -0500

    fully php app now, with the help of apache

--- html/index.php
+++ html/index.php
@@ -1,7 +1,5 @@
 <?php 
-
 	$url = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ."jokes";
-	/*
 	$curl = curl_init();
 	curl_setopt($curl, CURLOPT_URL, $url);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -12,30 +10,17 @@
     die('error occured during curl exec. Additioanl info: ' . var_export($info));
 	}
 	curl_close($curl);
-
-	echo $curl_response;
-	*/
+	$joke = json_decode($curl_response)[0];
 ?>
-
-
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
-    <script>
-	    var xmlHttp = new XMLHttpRequest();
-	    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-        	document.getElementById('joke').innerHTML = JSON.parse(xmlHttp.responseText)[0];
-	    }
-	    xmlHttp.open("GET", "/jokes", true);
-	    xmlHttp.send(null);
-    </script>
 </head>
 <body style="text-align: center;">
 	<div style="position: absolute; width: 100%;">
 		Your random joke:
-		<div id="joke">...loading</div>
+		<div id="joke"><?php echo $joke; ?></div>
 	</div>
 </body>
 </html>
